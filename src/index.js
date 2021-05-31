6bceaa655ed0a740b22077fe94a92402efbb3e8c
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-31 18:53:07 +0300

    Ver.1. No use Redux technology

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './styles.css'
+//---START___исходное приложение на JS состояние JS хранится внутри компонента
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const theme = document.getElementById('theme')
+
+let state = 0
+
+function render () {
+  counter.textContent = state.toString()
+}
+addBtn.addEventListener('click', () => {
+  state++
+  render()
+})
+subBtn.addEventListener('click', () => {
+  state--
+  render()
+})
+asyncBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    state++
+    render()
+  }, 2000)
+})
+theme.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
+render()
+//---START___исходное приложение на JS состояние JS хранится внутри компонента
